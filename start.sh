fc15554a34ec2b466f0c186906e0a0b7a1282a4c
Nozel Rosario <nozelrosario@gmail.com>
2024-08-05 12:22:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "This script is about to start MUDR24-302-Model-" $1 " training for " $2 "minutes"
 sh ./create-spot-instance.sh MUDR24-302-base "MUDR24-302-Model-{$1}" $2
-echo "This script has just finished model training"
+echo "This script has just done triggering model training"
